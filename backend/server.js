bcce6698e3075fa2cbe8653a78cc140787a71a1c
Jagroop Singh <jagroops2305@gmail.com>
2025-12-05 21:55:11 -0500

    Added upload route with Multer

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ app.use(express.static("public"));
 app.get("/api/getImage", (req, res) => {
     const name = req.query.name;
 
+    // adding more funtion 
+
     if (!name) {
         return res.status(400).json({ error: "No name provided" });
     }
@@ -22,6 +24,7 @@ app.get("/api/getImage", (req, res) => {
         res.send(`${name}.jpg`);
     } else {
         res.status(404).json({ error: "Image not found" });
+        //
     }
 });
 
@@ -33,12 +36,16 @@ const storage = multer.diskStorage({
     filename: (req, file, cb) => {
         const newName = req.query.name + ".jpg";
         cb(null, newName);
+        //
     }
 });
 
 const upload = multer({ storage });
 
 // --- UPLOAD IMAGE ROUTE ---
+//uploading image 
+//
+
 app.post("/api/upload", upload.single("image"), (req, res) => {
     if (!req.query.name) {
         return res.status(400).json({ error: "Name query required" });
@@ -48,6 +55,9 @@ app.post("/api/upload", upload.single("image"), (req, res) => {
 });
 
 // Start server
+//server is started 
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
 });
+
+//finnaly done
